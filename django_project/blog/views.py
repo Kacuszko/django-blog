ad672be6ac79f9ccb9e53385ad72a7e198c55d4a
kacuszko <kacuszko@gmail.com>
2020-03-08 21:44:18 +0100

    Added login functionality

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Mateusz Kacaliński',
-        'title': 'Thats post title',
-        'content': 'Thats post content',
-        'date_posted': 'August 28, 2020'
-    },
-    {
-        'author': 'Maria Dudziak',
-        'title': 'Another post title',
-        'content': 'Another post content',
-        'date_posted': 'August 30, 2020'
-    }
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
